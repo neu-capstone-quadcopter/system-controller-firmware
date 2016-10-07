1d79361250f84e650b4f9f914bf7b48a3f265338
bsoper <soper103@gmail.com>
2016-10-07 10:48:55 -0400

    Added monarcpb submodule and test for encoding a message.

--- src/tasks/src/nav_computer_task.cpp
+++ src/tasks/src/nav_computer_task.cpp
@@ -9,6 +9,9 @@
 #include "hal.hpp"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "pb_encode.h"
+#include "pb_decode.h"
+#include "api.pb.h"
 
 #define EVENT_QUEUE_DEPTH 8
 
@@ -33,6 +36,26 @@ void start() {
 
 static void task_loop(void *p) {
 	// UART initialization stuff.
+    uint8_t buffer[128];
+    size_t message_length;
+    bool status;
+
+    /* Encode our message */
+	/* Allocate space on the stack to store the message data.
+	 *
+	 * Nanopb generates simple struct definitions for all the messages.
+	 * - check out the contents of simple.pb.h!
+	 * It is a good idea to always initialize your structures
+	 * so that you do not have garbage data from RAM in there.
+	 */
+    monarcpb_SysCtrlToNavCPU_Telemetry message = {42};
+
+	/* Create a stream that will write to our buffer. */
+	pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+
+	/* Now we are ready to encode the message! */
+	status = pb_encode(&stream, monarcpb_NavCPUToSysCtrl_Telemetry_fields, &message);
+	message_length = stream.bytes_written;
 
 	nav_event_t current_event;
 	for(;;) {

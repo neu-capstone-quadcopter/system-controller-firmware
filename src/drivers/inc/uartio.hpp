68658758868f694d4643eca31fb1ad26a0c704d0
Nigil Lee <me@nigil.me>
2016-10-12 16:40:53 -0400

    began rewriting the uart driver interface

--- src/drivers/inc/uartio.hpp
+++ src/drivers/inc/uartio.hpp
@@ -21,30 +21,91 @@
 
 typedef void (*uart_char_read_callback)(uint8_t);
 
+enum UartTransferMode {POLLING, INTERRUPT, DMA};
+
+enum UartError {
+	NONE,
+	NO_DMA_CHANNEL
+};
+
 class UartIo : public Driver {
 public:
 	UartIo(LPC_USART_T *uart);
 	void init_driver(void);
 
-	inline void setBaud(uint32_t baud) {this->baud_rate = baud;
-	Chip_UART_SetBaud(this->uart, baud);}
-	void configData(uint32_t word_length, uint32_t parity, uint32_t stopbits);
-	void uartInterruptHandler(void);
+	/*
+	 * @brief Set the UART buad rate
+	 * @param baud : The baud rate to set
+	 * @return none
+	 */
+	void set_baud(uint32_t baud);
 
-	//Read and Write
-	void write(uint8_t* data, uint8_t length);
-	void read(uint8_t* data, uint8_t length);
+	/*
+	 * @brief Configure the mode in switch the uart sends data
+	 * @param word_length : The number of bits per word
+	 * @param parity : The number of parity bits to send
+	 * @param stop_bits : The number of stop bits to send
+	 * @return none
+	 */
+	void config_data_mode(uint32_t word_length, uint32_t parity, uint32_t stop_bits);
+
+	/*
+	 * @brief Set up the internal transmit transfer mode of the driver
+	 * @param mode : This is the mode of the transfer
+	 * @return NONE on success
+	 */
+	UartError set_tx_transfer_mode(UartTransferMode mode);
+
+	/*
+	 * @brief Set up the internal receive transfer mode of the driver
+	 * @param mode : This is the mode of the transfer
+	 * @return NONE on success
+	 */
+	UartError set_rx_transfer_mode(UartTransferMode mode);
+
+	UartError bind_tx_dma_channel(GpdmaChannel *dma_channel);
+	UartError unbind_tx_dma_channel(void);
+	UartError bind_rx_dma_channel(GpdmaChannel *dma_channel);
+	UartError unbind_rx_dma_channel(void);
+
+	/*
+	 * @brief Write bytes via the UART
+	 * @param data : Pointer to the data to be written
+	 * @param length : Length of the data this is being written
+	 * @return NONE on success
+	 * @note
+	 * The data will be send via whatever tx transfer mode you setup
+	 */
+	UartError write(uint8_t* data, uint8_t length);
+
+	/*
+	 * @brief Read bytes from UART
+	 * @param data : Pointer to where the data is to be saved
+	 * @param length : Length of the data to be read
+	 * @return NONE on success
+	 * @note
+	 * The data will be read via whatever rx transfer mode you setup
+	 */
+	UartError read(uint8_t* data, uint8_t length);
+
 	void write_dma(uint8_t* data, uint8_t length, GpdmaChannel *dma_channel);
 	inline void readChar(uint8_t* data) {read(data, 1);}
 	inline void writeChar(uint8_t data) {write(&data,1);}
 	void readCharAsync(uart_char_read_callback callback);
 
+	void uartInterruptHandler(void);
 private:
-	IRQn_Type getNVICIRQ(void);
+	IRQn_Type get_nvic_irq(void);
 	uint32_t get_tx_dmareq(void);
+
+	UartTransferMode tx_transfer_mode;
+	UartTransferMode rx_transfer_mode;
 
 	SemaphoreHandle_t tx_transfer_semaphore;
 	SemaphoreHandle_t rx_transfer_semaphore;
+
+	GpdmaChannel *tx_dma_channel = NULL;
+	GpdmaChannel *rx_dma_channel = NULL;
 
 	bool write_in_progress = false;
 	bool read_in_progress = false;
--- src/drivers/src/uartio.cpp
+++ src/drivers/src/uartio.cpp
@@ -11,10 +11,8 @@
 #include "FreeRTOS.h"
 #include "board.hpp"
 
-
+/* Default Values */
 #define DEFAULT_BAUD 115200
-
-//Data Config
 #define DEFAULT_WORD_LENGTH UART_LCR_WLEN8
 #define DEFAULT_PARITY UART_LCR_PARITY_DIS
 #define DEFAULT_STOP_BIT UART_LCR_SBS_1BIT
@@ -23,11 +21,138 @@ UartIo::UartIo(LPC_USART_T *uart) {
 	this->uart = uart;
 }
 
-void UartIo::configData(uint32_t word_length, uint32_t parity, uint32_t stopbits){
-	Chip_UART_ConfigData(this->uart, (word_length | stopbits));
+void UartIo::init_driver(void)
+{
+	board::uart_init(this->uart);
+	Chip_UART_Init(this->uart);
+
+	this->tx_transfer_semaphore = xSemaphoreCreateBinary();
+	this->rx_transfer_semaphore = xSemaphoreCreateBinary();
+
+	set_baud(DEFAULT_BAUD);
+
+	config_data_mode(DEFAULT_WORD_LENGTH, DEFAULT_PARITY, DEFAULT_STOP_BIT);
+
+	Chip_UART_TXEnable(this->uart);
+
+	RingBuffer_Init(&this->rxring, this->rxbuff, 1, RX_BUFFER_SIZE);
+	RingBuffer_Init(&this->txring, this->txbuff, 1, TX_BUFFER_SIZE);
+
+	Chip_UART_SetupFIFOS(this->uart, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2 |
+	                       UART_FCR_RX_RS | UART_FCR_TX_RS));
+
+	Chip_UART_IntEnable(this->uart, (UART_IER_RBRINT | UART_IER_RLSINT));
+	NVIC_EnableIRQ(get_nvic_irq());
+
+	//Can remove after testing...
+	//Chip_GPIO_WriteDirBit(LPC_GPIO,2,10,true);
 }
 
-IRQn_Type UartIo::getNVICIRQ(void){
+void UartIo::set_baud(uint32_t baud) {
+	this->baud_rate = baud;
+	Chip_UART_SetBaud(this->uart, baud);
+}
+
+void UartIo::config_data_mode(uint32_t word_length, uint32_t parity, uint32_t stop_bits){
+	Chip_UART_ConfigData(this->uart, (word_length | stop_bits));
+}
+
+UartError UartIo::set_tx_transfer_mode(UartTransferMode mode) {
+	if(mode == DMA && !this->tx_dma_channel) {
+		return NO_DMA_CHANNEL;
+	}
+	this->tx_transfer_mode = mode;
+	return NONE;
+}
+
+UartError UartIo::set_rx_transfer_mode(UartTransferMode mode) {
+	if(mode == DMA && !this->rx_dma_channel) {
+		return NO_DMA_CHANNEL;
+	}
+	this->rx_transfer_mode = mode;
+	return NONE;
+}
+
+UartError UartIo::bind_tx_dma_channel(GpdmaChannel *dma_channel) {
+	this->tx_dma_channel = dma_channel;
+	return NONE;
+}
+
+UartError UartIo::unbind_tx_dma_channel(void) {
+	this->tx_dma_channel = NULL;
+	if(this->tx_transfer_mode == DMA) {
+		this->tx_transfer_mode = INTERRUPT;
+	}
+	return NONE;
+}
+
+UartError UartIo::bind_rx_dma_channel(GpdmaChannel *dma_channel) {
+	this->rx_dma_channel = dma_channel;
+	return NONE;
+}
+
+UartError UartIo::unbind_rx_dma_channel(void) {
+	this->rx_dma_channel = NULL;
+	if(this->rx_transfer_mode == DMA) {
+		this->rx_transfer_mode = INTERRUPT;
+	}
+	return NONE;
+}
+
+UartError UartIo::write(uint8_t* data, uint8_t length)
+{
+	switch(this->tx_transfer_mode) {
+	case POLLING:
+		// TODO: Maybe implement this lol
+		break;
+	case INTERRUPT:
+		write_in_progress = true;
+		Chip_UART_SendRB(this->uart, &this->txring, data, length);
+		xSemaphoreTake(this->tx_transfer_semaphore, portMAX_DELAY);
+		return NONE;
+	case DMA:
+		return NONE;
+	}
+}
+
+UartError UartIo::read(uint8_t* data, uint8_t length)
+{
+	switch(this->rx_transfer_mode) {
+	case POLLING:
+		// TODO: Maybe implement this lol
+		break;
+	case INTERRUPT:
+		this->read_in_progress = true;
+		xSemaphoreTake(this->rx_transfer_semaphore, portMAX_DELAY);
+		Chip_UART_ReadRB(this->uart, &this->rxring, data, length);
+		return NONE;
+	case DMA:
+		return NONE;
+	}
+}
+
+void UartIo::write_dma(uint8_t* data, uint8_t length, GpdmaChannel *dma_channel) {
+	if(dma_channel->is_active()) {
+		// Error here
+	}
+
+	this->uart->FCR |= UART_FCR_DMAMODE_SEL;
+
+	dma_channel->start_transfer(
+			(uint32_t)data,
+			get_tx_dmareq(),
+			GPDMA_TRANSFERTYPE_M2P_CONTROLLER_DMA,
+			length);
+}
+
+void UartIo::readCharAsync(uart_char_read_callback callback)
+{
+	this->callback = callback;
+	this->async_read_in_progress = true;
+
+}
+
+IRQn_Type UartIo::get_nvic_irq(void){
 
 	switch((uint32_t)this->uart)
 	{
@@ -50,112 +175,6 @@ IRQn_Type UartIo::getNVICIRQ(void){
 	}
 }
 
-void UartIo::uartInterruptHandler(void){
-	Chip_UART_IRQRBHandler(this->uart, &this->rxring, &this->txring);
-
-	if(write_in_progress && RingBuffer_IsEmpty(&this->txring));
-	{
-		xSemaphoreGiveFromISR(this->tx_transfer_semaphore, NULL);
-		write_in_progress = false;
-	}
-
-	if(this->async_read_in_progress && !RingBuffer_IsEmpty(&this->rxring))
-	{
-		uint8_t data;
-		Chip_UART_ReadRB(this->uart, &this->rxring, &data, 1);
-		this->callback(data);
-		this->async_read_in_progress = false;
-
-	}
-
-	else if(this->read_in_progress)
-	{
-		if(!RingBuffer_IsEmpty(&this->rxring))
-		{
-			this->read_in_progress = false;
-			xSemaphoreGiveFromISR(this->rx_transfer_semaphore, NULL);
-		}
-	}
-}
-
-void UartIo::write(uint8_t* data, uint8_t length)
-{
-	//Write to the transmission buffer
-	write_in_progress = true;
-	Chip_UART_SendRB(this->uart, &this->txring, data, length);
-	xSemaphoreTake(this->tx_transfer_semaphore, portMAX_DELAY);
-
-}
-
-void UartIo::write_dma(uint8_t* data, uint8_t length, GpdmaChannel *dma_channel) {
-	if(dma_channel->is_active()) {
-		// Error here
-	}
-
-	this->uart->FCR |= UART_FCR_DMAMODE_SEL;
-
-	dma_channel->start_transfer(
-			(uint32_t)data,
-			get_tx_dmareq(),
-			GPDMA_TRANSFERTYPE_M2P_CONTROLLER_DMA,
-			length);
-}
-
-void UartIo::read(uint8_t* data, uint8_t length){
-	//Error Checking?
-
-	//Read from read buffer
-	this->read_in_progress = true;
-	xSemaphoreTake(this->rx_transfer_semaphore, portMAX_DELAY);
-
-	Chip_UART_ReadRB(this->uart, &this->rxring, data, length);
-}
-
-void UartIo::readCharAsync(uart_char_read_callback callback)
-{
-	this->callback = callback;
-	this->async_read_in_progress = true;
-
-}
-
-
-
-void UartIo::init_driver(void)
-{
-
-	//UART init
-	board::uart_init(this->uart);
-	Chip_UART_Init(this->uart);
-
-	//Semaphores
-	this->tx_transfer_semaphore = xSemaphoreCreateBinary();
-	this->rx_transfer_semaphore = xSemaphoreCreateBinary();
-
-	//Set Baud
-	setBaud(DEFAULT_BAUD);
-
-	//Configure our data frame
-	configData(DEFAULT_WORD_LENGTH, DEFAULT_PARITY, DEFAULT_STOP_BIT);
-
-	//Enable Transmission
-	Chip_UART_TXEnable(this->uart);
-
-	//Init Rx/Tx Ring Buffers
-	RingBuffer_Init(&this->rxring, this->rxbuff, 1, RX_BUFFER_SIZE);
-	RingBuffer_Init(&this->txring, this->txbuff, 1, TX_BUFFER_SIZE);
-
-	//FIFO stuff
-	Chip_UART_SetupFIFOS(this->uart, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2 |
-	                       UART_FCR_RX_RS | UART_FCR_TX_RS));
-	//Enable Interrupts
-	Chip_UART_IntEnable(this->uart, (UART_IER_RBRINT | UART_IER_RLSINT));
-	NVIC_EnableIRQ(getNVICIRQ());
-
-	//Can remove after testing...
-	Chip_GPIO_WriteDirBit(LPC_GPIO,2,10,true);
-
-}
-
 uint32_t UartIo::get_tx_dmareq(void) {
 	switch((uint32_t)this->uart)
 		{
@@ -176,4 +195,30 @@ uint32_t UartIo::get_tx_dmareq(void) {
 			configASSERT(0);
 
 		}
+}
+
+void UartIo::uartInterruptHandler(void){
+	Chip_UART_IRQRBHandler(this->uart, &this->rxring, &this->txring);
+
+	if(write_in_progress && RingBuffer_IsEmpty(&this->txring));
+	{
+		xSemaphoreGiveFromISR(this->tx_transfer_semaphore, NULL);
+		write_in_progress = false;
+	}
+
+	if(this->async_read_in_progress && !RingBuffer_IsEmpty(&this->rxring))
+	{
+		uint8_t data;
+		Chip_UART_ReadRB(this->uart, &this->rxring, &data, 1);
+		this->callback(data);
+		this->async_read_in_progress = false;
+	}
+	else if(this->read_in_progress)
+	{
+		if(!RingBuffer_IsEmpty(&this->rxring))
+		{
+			this->read_in_progress = false;
+			xSemaphoreGiveFromISR(this->rx_transfer_semaphore, NULL);
+		}
+	}
 }

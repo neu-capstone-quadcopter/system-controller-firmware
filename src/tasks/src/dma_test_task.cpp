75c828750f5cd4fe2d9fe97b202560a38b3cabc6
Nigil Lee <me@nigil.me>
2016-10-14 13:17:49 -0400

    async example works!

--- src/tasks/src/dma_test_task.cpp
+++ src/tasks/src/dma_test_task.cpp
@@ -21,18 +21,16 @@
 namespace dma_test_task {
 	static void task_loop(void *p);
 	static void read_handler(std::shared_ptr<UartReadData> data);
+	static void write_handler(UartError status);
 
 	TaskHandle_t task_handle;
-	SemaphoreHandle_t semphr;
 	UartIo *uart;
 	GpdmaManager *dma_man;
 	GpdmaChannel *test_channel_tx;
 	GpdmaChannel *test_channel_rx;
 
-	const char *str = "test\r\n";
-	uint8_t str_len = 6;
-
-	std::unique_ptr<uint8_t[]> last_read;
+	auto read_del = dlgt::make_delegate(&read_handler);
+	auto write_del = dlgt::make_delegate(&write_handler);
 
 	void start(void) {
 		uart = hal::get_driver<UartIo>(hal::CONSOLE_UART);
@@ -42,31 +40,41 @@ namespace dma_test_task {
 		test_channel_tx = dma_man->allocate_channel(0);
 		test_channel_rx = dma_man->allocate_channel(1);
 
-		//uart->bind_dma_channels(test_channel_tx, test_channel_rx);
-		//uart->set_transfer_mode(UART_XFER_MODE_DMA);
-
-		semphr = xSemaphoreCreateBinary();
+		// Enabled DMA (Optional)
+		uart->bind_dma_channels(test_channel_tx, test_channel_rx);
+		uart->set_transfer_mode(UART_XFER_MODE_DMA);
 
 		xTaskCreate(task_loop, "dma task", 1536, NULL, 2, &task_handle);
 	}
 
 	static void task_loop(void *p) {
+		/*
+		// Sync example
 		uint8_t array[6] = {'t', 'e', 's', 't', '\r', '\n'};
 		while(true) {
-			//auto del = dlgt::make_delegate(&read_handler);
-			//uart->read_async(5, del);
-			//xSemaphoreTake(semphr, portMAX_DELAY);
-			//volatile uint8_t* d = last_read.get();
-			//uart->write((uint8_t*)d, 5);
+
 
 			uart->write(array, 6);
 			uart->read(array, 6);
 		}
+		*/
+
+		// Async example
+		uart->read_async(5, read_del);
+		while(true) {
+			asm("nop;");
+		}
 	}
 
 	static void read_handler(std::shared_ptr<UartReadData> read_status) {
-		last_read = std::move(read_status->data);
-		xSemaphoreGive(semphr);
+		std::unique_ptr<uint8_t[]> read_data = std::move(read_status->data);
+		uint8_t *data_ptr = read_data.get();
+
+		uart->write_async(data_ptr, read_status->length, write_del);
+	}
+
+	static void write_handler(UartError status) {
+		uart->read_async(5, read_del);
 	}
 }
 

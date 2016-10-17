845fdf71abe508a30203f570b5cc9b2c49a927ac
bsoper <soper103@gmail.com>
2016-10-17 17:30:58 -0400

    Updated to populate protobuf with adc data.

--- src/tasks/src/nav_computer_task.cpp
+++ src/tasks/src/nav_computer_task.cpp
@@ -23,6 +23,7 @@ namespace nav_computer_task {
 
 static void task_loop(void *p);
 void send_data(sensor_task::adc_values_t data);
+void package_data(sensor_task::adc_values_t data, monarcpb_SysCtrlToNavCPU &message);
 void write_to_uart(uint8_t *data, uint16_t len);
 void read_from_uart();
 
@@ -75,17 +76,19 @@ void read_from_uart() {
 	int32_t gps = message.telemetry.GPS;
 }
 
-
 void send_data(sensor_task::adc_values_t data) {
 	static uint8_t buffer[MAX_BUFFER_SIZE];
 	memset(buffer, 0x00, MAX_BUFFER_SIZE);
 
 	monarcpb_SysCtrlToNavCPU message = monarcpb_SysCtrlToNavCPU_init_zero;
+	package_data(data, message);
+	/*
 	message.has_telemetry = true;
 	message.telemetry.has_altitude = true;
 	message.telemetry.altitude = data.sensor_values[8];
 	message.telemetry.has_atmospheric_pressure = true;
 	message.telemetry.atmospheric_pressure = data.sensor_values[13];
+	*/
 
 	/* Create a stream that will write to our buffer. */
 	pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
@@ -95,6 +98,81 @@ void send_data(sensor_task::adc_values_t data) {
 	uint16_t message_len = stream.bytes_written;
 
 	write_to_uart(buffer, message_len);
+	return;
+}
+
+void package_data(sensor_task::adc_values_t data, monarcpb_SysCtrlToNavCPU &message) {
+	int i = 0;
+	message.has_analog_sensors = true;
+	while (i < 16) {
+		switch (i) {
+		case 0:
+			message.analog_sensors.has_sys_3v3_isense = true;
+			message.analog_sensors.sys_3v3_isense = data.sensor_values[i];
+			break;
+		case 1:
+			message.analog_sensors.has_sys_3v3_isense = true;
+			message.analog_sensors.sys_5v_isense = data.sensor_values[i];
+			break;
+		case 2:
+			message.analog_sensors.has_vgps_isense = true;
+			message.analog_sensors.vgps_isense = data.sensor_values[i];
+			break;
+		case 3:
+			message.analog_sensors.has_vusb_isense = true;
+			message.analog_sensors.vusb_isense = data.sensor_values[i];
+			break;
+		case 4:
+			message.analog_sensors.has_vfltctl_isense = true;
+			message.analog_sensors.vfltctl_isense = data.sensor_values[i];
+			break;
+		case 5:
+			message.analog_sensors.has_navcmp_isense = true;
+			message.analog_sensors.navcmp_isense = data.sensor_values[i];
+			break;
+		case 6:
+			message.analog_sensors.has_vradio_isense = true;
+			message.analog_sensors.vradio_isense = data.sensor_values[i];
+			break;
+		case 7:
+			message.analog_sensors.has_tp27 = true;
+			message.analog_sensors.tp27 = data.sensor_values[i];
+			break;
+		case 8:
+			message.analog_sensors.has_vradio_vsense = true;
+			message.analog_sensors.vradio_vsense = data.sensor_values[i];
+			break;
+		case 9:
+			message.analog_sensors.has_navcmp_vsense = true;
+			message.analog_sensors.navcmp_vsense = data.sensor_values[i];
+			break;
+		case 10:
+			message.analog_sensors.has_vusb_vsense = true;
+			message.analog_sensors.vusb_vsense = data.sensor_values[i];
+			break;
+		case 11:
+			message.analog_sensors.has_vfltctl_vsense = true;
+			message.analog_sensors.vfltctl_vsense = data.sensor_values[i];
+			break;
+		case 12:
+			message.analog_sensors.has_vgps_vsense = true;
+			message.analog_sensors.vgps_vsense = data.sensor_values[i];
+			break;
+		case 13:
+			message.analog_sensors.has_sys_5v_vsense = true;
+			message.analog_sensors.sys_5v_vsense = data.sensor_values[i];
+			break;
+		case 14:
+			message.analog_sensors.has_vsys_vsense = true;
+			message.analog_sensors.vsys_vsense = data.sensor_values[i];
+			break;
+		case 15:
+			message.analog_sensors.has_sys_3v3_vsense = true;
+			message.analog_sensors.sys_3v3_vsense = data.sensor_values[i];
+			break;
+		}
+		i++;
+	}
 	return;
 }
 
